a6dba969d4121d9ac7bceb3d9488e6b78688a945
Dmytro Tkachenko <dimatkachenkopro@gmail.com>
2019-04-19 10:50:32 +0200

    Updates

---
+++ precache-manifest.e618d413fab031eef1683549379d081c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/dimatkachenkopro/weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "4048d1ee84f7cc1a8660",
+    "url": "/dimatkachenkopro/weather-app/static/css/main.bd42a90b.chunk.css"
+  },
+  {
+    "revision": "9a5d1bc9aa4a4b1fc578",
+    "url": "/dimatkachenkopro/weather-app/static/js/runtime~main.80e26441.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/dimatkachenkopro/weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "22d15d75bf728aff999d",
+    "url": "/dimatkachenkopro/weather-app/static/js/2.66b5db03.chunk.js"
+  },
+  {
+    "revision": "e160b91c23ddec0625e979856b0b77ba",
+    "url": "/dimatkachenkopro/weather-app/static/media/bg.e160b91c.jpg"
+  },
+  {
+    "revision": "4048d1ee84f7cc1a8660",
+    "url": "/dimatkachenkopro/weather-app/static/js/main.91e671d9.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/dimatkachenkopro/weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/dimatkachenkopro/weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/dimatkachenkopro/weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "22d15d75bf728aff999d",
+    "url": "/dimatkachenkopro/weather-app/static/css/2.eb7891f7.chunk.css"
+  },
+  {
+    "revision": "0d17fd36068b97b227e2721dc302d129",
+    "url": "/dimatkachenkopro/weather-app/index.html"
+  }
+];
